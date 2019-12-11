a1e7d7a6323d26f0cec59455d1c1607e9561de49
holistudio <daniel.bin.lu@gmail.com>
2019-12-11 06:22:49 -0500

    initial code for generating human3.6m standard 2d detections

--- application.py
+++ application.py
@@ -16,3 +16,6 @@ Session(app)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- static/sketch.js
+++ static/sketch.js
@@ -1,4 +1,4 @@
-var keyPtsOfInterest = [0,5,9,13,15,16,14,10,6];
+var keyPtsOfInterest = [0,5,6,7,8,9,10,11,12,13,14,15,16];
 let spacing = 10;
 let drawFrame = 0;
 let numDrawFrames = 10;
@@ -30,8 +30,9 @@ function mouseClicked() {
 
 function keyPressed() {
   if (value === 0) {
-    writer = createWriter('extract.obj');
+    writer = createWriter('array.csv');
     video.play();
+    video.hide();
     value = 255;
   } else {
     value = 0;
@@ -58,14 +59,27 @@ function setup() {
   // video.play();
   // video.hide();
 
-  noFill();
+  fill(255);
   // var writer = createWriter('extract.obj');
+}
+
+function midPoint(point1, point2){
+  let mid = {"x": 0, "y":0}
+  mid.x = (point1.x + point2.x) / 2;
+  mid.y = (point1.y + point2.y) / 2;
+  return mid;
+}
+
+function slope(point1, point2){
+  let sl = (point2.y - point1.y) / (point2.x - point1.x);
+
+  return sl;
 }
 
 function draw() {
 
   background(0);
-  // image(video, 0, 0, video.width, video.height);
+  image(video, 0, 0, video.width, video.height);
 
   if(poses.length>=1){
     //for each pose detected
@@ -88,60 +102,87 @@ function draw() {
     }
 
 
-    if(frameCount % objRecordRate == 0){
-      //if keyShape has more than 2 positions
-      if(keyShape.length>2){
-        //record keyShape's positions
-        for (let i = 0; i < keyShape.length; i++) {
-          writer.print(`v ${objRecordX} ${keyShape[i].x} ${video.height-keyShape[i].y}`);
-        }
-        //vt vn and f lines according to keyShape's length
-        //vn 1 0 0 for every vertex
-        //vt 1 1 for every vertex
-        for (let i = 0; i < keyShape.length; i++) {
-          writer.print(`vt 1 1`);
-        }
-        for (let i = 0; i < keyShape.length; i++) {
-          writer.print(`vn 1 0 0`);
-        }
-        let n = keyVertex;
-        //number of faces equals number of vertices -2 (keyShape's length - 2)
-        for (let i = 0; i < keyShape.length-2; i++) {
-          writer.print(`f ${n+1}/${n+1}/${n+1} ${n+2}/${n+2}/${n+2} ${keyVertex}/${keyVertex}/${keyVertex}`);
-          n++;
-        }
-        keyVertex+= keyShape.length;
-      }
-      //always increment objRecordX by objRecordSpacing (so blank/invalid shapes will lead to blanks in the obj record)
-      objRecordX = objRecordX + objRecordSpacing;
 
-    }
 
-    //if poseShapes length is less than numDrawFrames
-    if(poseShapes.length<numDrawFrames){
-      //add shape to end of array, as an array of positions
-      poseShapes.push(keyShape);
-    }
+    if(keyShape.length == keyPtsOfInterest.length){
 
-    //if the shapeArray length is numDrawFrames, draw all shapes
-    if(poseShapes.length == numDrawFrames){
+      //calculate hip, neck, and head points
+      //hip, midpoint of keypoints 11 and 12
+      keyShape.push(midPoint(keyShape[11],keyShape[12]));
+      //neck, midpoint of keypoints 5 and 6
+      keyShape.push(midPoint(keyShape[1],keyShape[2]));
+      //head is double the distance bewteen nose and neck
+      keyShape.push(keyShape[keyShape.length-1]+2*slope(keyShape[keyShape.length-1],keyShape[0]));
 
-      let shade = 0.1;
-
-      for (let k = 0; k<poseShapes.length; k++){
-        stroke(`rgba(255,255,255,${shade})`);
-        beginShape();
-        for (let l = 0; l<poseShapes[k].length; l++){
-          vertex(poseShapes[k][l].x-(numDrawFrames*spacing), poseShapes[k][l].y);
-
-        }
-        endShape(CLOSE);
-        translate(spacing,0);
-        shade+= 0.2;
+      //draw circles showing detected parts
+      for (let i = 0; i < keyShape.length; i++) {
+        circle(keyShape[i].x, keyShape[i].y, 5);
       }
 
-      //remove shapeArray's first element (oldest)
-      poseShapes.splice(0, 1);
+      //record positions into the csv file
+      // for (let i = 0; i < keyShape.length; i++) {
+      //   writer.print(`v ${objRecordX} ${keyShape[i].x} ${video.height-keyShape[i].y}`);
+      // }
     }
+    else{
+      console.log(frameCount);
+    }
+
+
+    // if(frameCount % objRecordRate == 0){
+    //   //if keyShape has more than 2 positions
+    //   if(keyShape.length>2){
+    //     //record keyShape's positions
+    //     for (let i = 0; i < keyShape.length; i++) {
+    //       writer.print(`v ${objRecordX} ${keyShape[i].x} ${video.height-keyShape[i].y}`);
+    //     }
+    //     //vt vn and f lines according to keyShape's length
+    //     //vn 1 0 0 for every vertex
+    //     //vt 1 1 for every vertex
+    //     for (let i = 0; i < keyShape.length; i++) {
+    //       writer.print(`vt 1 1`);
+    //     }
+    //     for (let i = 0; i < keyShape.length; i++) {
+    //       writer.print(`vn 1 0 0`);
+    //     }
+    //     let n = keyVertex;
+    //     //number of faces equals number of vertices -2 (keyShape's length - 2)
+    //     for (let i = 0; i < keyShape.length-2; i++) {
+    //       writer.print(`f ${n+1}/${n+1}/${n+1} ${n+2}/${n+2}/${n+2} ${keyVertex}/${keyVertex}/${keyVertex}`);
+    //       n++;
+    //     }
+    //     keyVertex+= keyShape.length;
+    //   }
+    //   //always increment objRecordX by objRecordSpacing (so blank/invalid shapes will lead to blanks in the obj record)
+    //   objRecordX = objRecordX + objRecordSpacing;
+    //
+    // }
+
+    // //if poseShapes length is less than numDrawFrames
+    // if(poseShapes.length<numDrawFrames){
+    //   //add shape to end of array, as an array of positions
+    //   poseShapes.push(keyShape);
+    // }
+    //
+    // //if the shapeArray length is numDrawFrames, draw all shapes
+    // if(poseShapes.length == numDrawFrames){
+    //
+    //   let shade = 0.1;
+    //
+    //   for (let k = 0; k<poseShapes.length; k++){
+    //     stroke(`rgba(255,255,255,${shade})`);
+    //     beginShape();
+    //     for (let l = 0; l<poseShapes[k].length; l++){
+    //       vertex(poseShapes[k][l].x-(numDrawFrames*spacing), poseShapes[k][l].y);
+    //
+    //     }
+    //     endShape(CLOSE);
+    //     translate(spacing,0);
+    //     shade+= 0.2;
+    //   }
+    //
+    //   //remove shapeArray's first element (oldest)
+    //   poseShapes.splice(0, 1);
+    // }
   }
 }
