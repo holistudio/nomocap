1eb4e70dd8c50eaab0fba0e1ecdfba8652b9e17e
holistudio <daniel.bin.lu@gmail.com>
2019-12-21 19:42:57 -0500

    output filename change

--- Pose_3D/input_files/h5converter.py
+++ Pose_3D/input_files/h5converter.py
@@ -4,7 +4,7 @@ import h5py
 
 with open('arrays.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
-    with h5py.File("mytestfile.h5", "w") as f:
+    with h5py.File("preds.h5", "w") as f:
         dset = f.create_dataset("enc_in", (68,64), dtype='<f8')
         i=0;
         for row in reader:
