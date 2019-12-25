d4a28461f8786cc77956fc774ae8e6aadc0f3add
holistudio <daniel.bin.lu@gmail.com>
2019-12-24 19:40:31 -0500

    h5converter fix: accounts for size of csv instead of hardcode

--- Pose_3D/input_files/h5converter.py
+++ Pose_3D/input_files/h5converter.py
@@ -4,8 +4,9 @@ import h5py
 
 with open('arrays.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+    row_count = sum(1 for row in reader)
     with h5py.File("preds.h5", "w") as f:
-        dset = f.create_dataset("enc_in", (68,64), dtype='<f8')
+        dset = f.create_dataset("enc_in", (row_count,64), dtype='<f8')
         i=0;
         for row in reader:
             row = np.array(row);
