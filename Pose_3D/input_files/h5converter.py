f5bf91905b160a49c6bf630b3d99f678c42af085
holistudio <daniel.bin.lu@gmail.com>
2020-09-27 00:59:11 -0400

    updated h5converter

--- Pose_3D/input_files/h5converter.py
+++ Pose_3D/input_files/h5converter.py
@@ -1,14 +1,16 @@
 import csv
 import numpy as np
 import h5py
+import pandas as pd
 
 with open('arrays.csv', newline='') as csvfile:
+    row_count = sum(1 for row in csvfile);
+with open('arrays.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
-    row_count = sum(1 for row in reader)
     with h5py.File("preds.h5", "w") as f:
         dset = f.create_dataset("enc_in", (row_count,64), dtype='<f8')
         i=0;
         for row in reader:
-            row = np.array(row);
-            dset[i] = row;
+            dset[i] = np.array(row).astype(np.float);
             i=i+1;
+            print(i);
